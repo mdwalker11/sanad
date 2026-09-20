b269b0a950d7405207a747ebc7a1525a72b28b0c
Mdwalker11 <mdwalker092@gmail.com>
2026-09-20 18:40:00 +0000

    chore: clean order list lint

--- lib/orders/customer_orders_page.dart
+++ lib/orders/customer_orders_page.dart
@@ -65,7 +65,7 @@ class _CustomerOrdersPageState extends State<CustomerOrdersPage> {
               child: ListView.separated(
                 padding: const EdgeInsets.all(16),
                 itemCount: orders.length,
-                separatorBuilder: (_, __) => const SizedBox(height: 10),
+                separatorBuilder: (_, _) => const SizedBox(height: 10),
                 itemBuilder: (context, index) {
                   final order = orders[index];
                   return Card(
